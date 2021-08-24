0fe76ad27a58f4d935982bc16b76345af755ee11
Gilles Kagarama <kagaramag@gmail.com>
2021-08-24 21:44:55 +0200

    refactoring codes

--- src/components/framework/index.tsx
+++ src/components/framework/index.tsx
@@ -3,7 +3,6 @@ import { CodeHighlighter } from "../common/highlighter";
 
 const MDInstallYarn = `yarn add litesome`;
 const MDInstallNpm = `npm i litesome --save`;
-const MDInstallCDN = `<link rel="stylesheet" href="https://unpkg.com/litesome/css/litesome.css" type="text/css">`;
 
 const Utilities = () => {
   return (
@@ -13,16 +12,13 @@ const Utilities = () => {
         <div className="lead">
           Litesome CSS Framework is collection of CSS styles to consistently
           make simple and clean websites. It makes browsers render all elements
-          faster, beautifies semantic HTML markups and in line with modern
-          standards
+          faster and beautifies semantic HTML markups
         </div>
         <h3>How to install?</h3>
         <h4>Manually(recommended)</h4>
         <CodeHighlighter code={MDInstallYarn} />
         <div>or</div>
         <CodeHighlighter code={MDInstallNpm} />
-        <h4>CDN</h4>
-        <CodeHighlighter code={MDInstallCDN} />
       </DocLayout>
     </>
   );
